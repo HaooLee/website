3720e7f43bc94be571d5185692b984403750adc6
liushuai <liushuai@teddymobile.cn>
2020-03-13 21:27:09 +0800

    sms

--- components/problemItem/index.js
+++ components/problemItem/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import styles from './problem.less'
+import styles from './index.less'
 export default class ProblemItem extends React.Component {
   constructor(props){
     super(props)
   }
   render () {
-    const {src, title} = this.props
+    const {src, title, num = 5} = this.props
     return (
       <>
-        <div className="problem-item">
+        <div className="problem-item" style={{width: (100 / num) + '%'}}>
           <div className="problem-item__content">
             <img src={src} />
           </div>
-          <p className="problem-item__text">{title}</p>
+          <p className="problem-item__text">
+            <span>{title}</span>
+          </p>
         </div>
         <style jsx>{styles}</style>
       </>
--- pages/intelligenceSms/index.js
+++ pages/intelligenceSms/index.js
@@ -4,62 +4,76 @@ import SectionCard from '@/components/sectionCard'
 import ProblemItem from '@/components/problemItem'
 import styles from './index.less'
 
-export default class ScenicProduct extends React.Component {
+export default class IntelligenceSms extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       problems: [
         {
-          src: '/static/images/scenicProduct/icon-chengben.png',
-          title: '成本高'
+          src: '/static/images/intelligenceSms/icon-renzheng.png',
+          title: '短信未被认证 被拦截率高'
         },
         {
-          src: '/static/images/scenicProduct/icon-neirong.png',
-          title: '内容受字符限制'
+          src: '/static/images/intelligenceSms/icon-fuza.png',
+          title: '内容繁杂 用户打开率低 '
         },
         {
-          src: '/static/images/scenicProduct/icon-lanjie.png',
-          title: '被拦截率高'
+          src: '/static/images/intelligenceSms/icon-gongneng.png',
+          title: '通知类短信 功能单一 '
         },
         {
-          src: '/static/images/scenicProduct/icon-tuiguang.png',
-          title: '推广效果差'
+          src: '/static/images/intelligenceSms/icon-zifu.png',
+          title: '短信内容篇幅 受到字符限制 '
         },
         {
-          src: '/static/images/scenicProduct/icon-tiyancha.png',
-          title: '用户体验差'
+          src: '/static/images/intelligenceSms/icon-zhuanhua.png',
+          title: '短信营销转化 效果差 '
+        },
+        {
+          src: '/static/images/intelligenceSms/icon-fasong.png',
+          title: '营销短信 发送量受限'
         }
       ],
       products: [
         {
-          src: '/static/images/scenicProduct/cost.png',
-          title: '节省成本',
-          desc: '消息升级，价格更低，节省短信成本。'
+          src: '/static/images/intelligenceSms/icon-accurate.png',
+          title: '精确度高',
+          desc: '拥有独创的短信语义识别引擎。'
         },
         {
-          src: '/static/images/scenicProduct/limit.png',
-          title: '突破限制',
-          desc: '突破传统短信字符限制。'
+          src: '/static/images/intelligenceSms/icon-sample.png',
+          title: '样本丰富',
+          desc: '每天解析32亿条短信。'
         },
         {
-          src: '/static/images/scenicProduct/effect.png',
-          title: '提高效率',
-          desc: '降低短信拦截率、投诉率。'
+          src: '/static/images/intelligenceSms/icon-service.png',
+          title: '精准提供服务',
+          desc: '能够基于自然语义解析技术，根据用户场景，精准提供服务。'
         },
         {
-          src: '/static/images/scenicProduct/remain.png',
-          title: '短信留存',
-          desc: '消息留存在短信列表中。'
+          src: '/static/images/intelligenceSms/icon-cover.png',
+          title: '高效覆盖',
+          desc: '号码、卡片、内容精准识别。'
         },
         {
-          src: '/static/images/scenicProduct/marketing.png',
-          title: '精准营销',
-          desc: '场景触发，营销更精准。'
+          src: '/static/images/intelligenceSms/icon-put-in.png',
+          title: '投放可控',
+          desc: '投放可控、稳定、快速。'
         },
         {
-          src: '/static/images/scenicProduct/experience.png',
-          title: '体验提升',
-          desc: '富媒体消息，营销短信多元化， 用户体验提升，功能承载更多。'
+          src: '/static/images/intelligenceSms/icon-private.png',
+          title: '隐私保护',
+          desc: '符合GDPR要求，保护用户隐私。'
+        },
+        {
+          src: '/static/images/intelligenceSms/icon-intercept.png',
+          title: '降低拦截率',
+          desc: '文案、模板预审流程，降低拦截率。'
+        },
+        {
+          src: '/static/images/intelligenceSms/icon-change.png',
+          title: '提升转化率',
+          desc: '一键直达，缩短用户办理路径，提升转化率。'
         }
       ]
     }
@@ -71,16 +85,16 @@ export default class ScenicProduct extends React.Component {
         <section className="banner">
             <div className="banner-content">
               <div className="banner__desc">
-                <h1 className="banner__desc__title">场景消息</h1>
-                <p className="banner__desc__content">场景消息——千人千面的智慧场景营销服务</p>
+                <h1 className="banner__desc__title">智能短信</h1>
+                <p className="banner__desc__content">带你了解短信新形态，体验营销新玩法</p>
               </div>
-              <img className="banner__img" src="/static/images/scenicProduct/banner.png" />
+              <img className="banner__img" src="/static/images/intelligenceSms/banner.png" />
             </div>
         </section>
         <SectionCard title={'你的场景消息通常会遇到的问题'} bgc={"#fff"} >
           <div className="w clearfix problem">
             {
-              problems.map((item, index) => <ProblemItem src={item.src} title={item.title} key={index}></ProblemItem>)
+              problems.map((item, index) => <ProblemItem src={item.src} num={problems.length} title={item.title}></ProblemItem>)
             }
           </div>
         </SectionCard>
@@ -111,7 +125,7 @@ export default class ScenicProduct extends React.Component {
 
         </SectionCard>
         <SectionCard title={'产品优势'} bgc={'#F8FAFF'}>
-            <div className="product">
+            <div className="product clearfix">
               {products.map(item => (
                 <div className="product__item">
                   <div className="product__item-content">
