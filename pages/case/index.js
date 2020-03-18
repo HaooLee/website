6af0e9b3c5eb59053252f6bcca78e89704a27a5f
liushuai <liushuai@teddymobile.cn>
2020-03-18 19:06:25 +0800

    website

---
+++ pages/case/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+import styles from './index.less'
+
+export default class Case extends Component {
+  render() {
+    return (
+      <>
+        <div className="banner">
+          <div>banner</div>
+        </div>
+      </>
+    )
+  }
+}
