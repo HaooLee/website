9e2699c24e5888cfe2c95b7418d2452312df27f2
liushuai <liushuai@teddymobile.cn>
2020-03-24 11:34:38 +0800

    websit

--- pages/numberComplain/index.js
+++ pages/numberComplain/index.js
@@ -2,6 +2,9 @@ import {Component} from 'react'
 import styles from './index.less'
 
 export default class NumberComplain extends Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       <>
@@ -15,19 +18,85 @@ export default class NumberComplain extends Component {
           <div className="complain w">
             <div className="complain__header clearfix">
               <div className="complain__header__item active">
-              企业号码申诉通道
+                企业号码申诉通道
               </div>
               <div className="complain__header__item">
-              个人用户号码申诉通道
+                个人用户号码申诉通道
               </div>
             </div>
             <div className="complain__content">
               <div className="form">
-                <div className="form__item clearfix">
+                <div className="form__item">
                   <div className="form__item__label">申诉号码</div>
                   <div className="form__item__input">
-                    <input type="text" />
+                    <input placeholder="输入号码，如果是座机号请加上区号" type="text" />
                   </div>
+                </div>
+                <div className="form__item">
+                  <div className="form__item__label">申诉人手机号</div>
+                  <div className="form__item__input">
+                    <input placeholder="请留下您最常用的手机号，以便我们及时联系并申诉" type="text" />
+                  </div>
+                </div>
+                <div className="form__item">
+                  <div className="form__item__label">验证码</div>
+                  <div className="form__item__input">
+                    <input placeholder="请输入您的手机验证码" type="text" />
+                    <a className="form__item__input__code">获取验证码</a>
+                  </div>
+                </div>
+                <div className="form__item">
+                  <div className="form__item__label">添加证明</div>
+                  <div className="form__item__input form__item__input--v">
+                    <div className="form__item__input__item">
+                      <input placeholder="身份证正面照" type="text" />
+                      <div className="upload">
+                        <img src="/static/images/numberSign/upload-icon.png" />
+                        <span>选择文件</span>
+                      </div>
+                    </div>
+                    <div className="form__item__input__item">
+                      <input placeholder="号码归属证明" type="text" />
+                      <div className="upload">
+                        <img src="/static/images/numberSign/upload-icon.png" />
+                        <span>选择文件</span>
+                      </div>
+                    </div>
+                    <div className="form__item__input__item">
+                      <input placeholder="其他证明" type="text" />
+                      <div className="upload">
+                        <img src="/static/images/numberSign/upload-icon.png" />
+                        <span>选择文件</span>
+                      </div>
+                    </div>
+                  </div>  
+                </div>
+                <div className="form__item">
+                  <div className="form__item__label">申诉企业全称</div>
+                  <div className="form__item__input">
+                    <input placeholder="请输入您的企业全称" type="text" />
+                  </div>
+                </div>
+                <div className="form__item">
+                  <div className="form__item__label">申诉原因</div>
+                  <div className="form__item__input">
+                    <input placeholder="请说明您的申诉原因" type="text" />
+                  </div>
+                </div>
+                <div className="form__item">
+                  <div className="form__item__label">联系方式</div>
+                  <div className="form__item__input">
+                    <input placeholder="请留下您的联系电话，以便我们能够及时为您提供服务" type="text" />
+                  </div>
+                </div>
+                <div className="form__item">
+                  <div className="form__item__label">联系邮箱/QQ</div>
+                  <div className="form__item__input">
+                    <input placeholder="请留下您的联系邮箱及QQ" type="text" />
+                  </div>
+                </div>
+                <div className="form__item">
+                  <div className="form__item__btn">确认提交</div>
                 </div>
               </div>
             </div>
