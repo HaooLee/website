e5c30a4b3681ff1027d587d0815d7cad358ec609
lihao <lihao@fanqiepinpin.com>
2020-04-07 02:00:57 +0800

    fix bug

--- pages/news/index.js
+++ pages/news/index.js
@@ -6,44 +6,11 @@ import axios from 'axios'
 export default class News extends Component {
   static async getInitialProps({Component, router, ctx}) {
     const {data} = await axios.get('http://php.bjdglt.com:8091/V1.4/news/getinfo')
-    console.log(data.data)
     return {news: data.data}
   }
   constructor(props) {
     super(props)
     this.state = {
-      news: [
-        {
-          src: '/static/images/news/company.png',
-          title: '开发者大会',
-          desc: '开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会'
-        },
-        {
-          src: '/static/images/news/company.png',
-          title: '开发者大会',
-          desc: '开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会'
-        },
-        {
-          src: '/static/images/news/company.png',
-          title: '开发者大会',
-          desc: '开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会'
-        },
-        {
-          src: '/static/images/news/company.png',
-          title: '开发者大会',
-          desc: '开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会'
-        },
-        {
-          src: '/static/images/news/company.png',
-          title: '开发者大会',
-          desc: '开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会'
-        },
-        {
-          src: '/static/images/news/company.png',
-          title: '开发者大会',
-          desc: '开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会开发者大会'
-        }
-      ],
       list: [
         {
           name: '资讯列表',
@@ -93,26 +60,28 @@ export default class News extends Component {
           <SliderList list={list} clickCallback={this.clickCallback}></SliderList>
           <ul className="news__content col-10">
             {news.map((item, idx) => (
-              <li className="news__content__item clearfix" key={idx}>
-                <img className="news__content__item__img" src={'/static/images/news/company.png'} />
-                <div className="news__content__item__info">
-                  <p className="news__content__item__info__title">
-                    <Link href={`/newsDetail?id=${item.nid}`}>
-                      <a style={{color: '#000'}}>{item.news_title}</a>
-                    </Link>
-                  </p>
-                  <p className="news__content__item__info__desc">{item.news_des}</p>
-                  <div className="news__content__item__info__footer">
-                    <img src="/static/images/news/logo.png" />
-                    <span>泰迪资讯 | {item.news_time.replace(/\s[\x00-\xff]*/g,'')} </span>
-                  </div>
-                  <div className="news__content__item__info__footer--match">
-                    <img src="/static/images/news/logo.png" />
-                    <span className="news__content__item__info__footer--match__teddy">泰迪资讯</span>
-                    <span className="news__content__item__info__footer--match__time">{item.news_time.replace(/\s[\x00-\xff]*/g,'')}</span>
-                  </div>
-                </div>
-              </li>
+              <Link href={`/newsDetail?id=${item.nid}`} passHref>
+                <a>
+                  <li className="news__content__item clearfix" key={idx}>
+                    <img className="news__content__item__img" src={'/static/images/news/company.png'} />
+                    <div className="news__content__item__info">
+                      <p className="news__content__item__info__title">
+                        <p style={{color: '#000'}}>{item.news_title}</p>
+                      </p>
+                      <p className="news__content__item__info__desc">{item.news_des}</p>
+                      <div className="news__content__item__info__footer">
+                        <img src="/static/images/news/logo.png" />
+                        <span>泰迪资讯 | {item.news_time.replace(/\s[\x00-\xff]*/g,'')} </span>
+                      </div>
+                      <div className="news__content__item__info__footer--match">
+                        <img src="/static/images/news/logo.png" />
+                        <span className="news__content__item__info__footer--match__teddy">泰迪资讯</span>
+                        <span className="news__content__item__info__footer--match__time">{item.news_time.replace(/\s[\x00-\xff]*/g,'')}</span>
+                      </div>
+                    </div>
+                  </li>
+                </a>
+              </Link>
             ))}
             {/* <div className="load-more">加载更多</div> */}
           </ul>
