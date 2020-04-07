62bcbdf3493200217a63ff7eaff8ac55d680acb5
lihao <lihao@fanqiepinpin.com>
2020-04-07 12:15:31 +0800

    restart.sh

---
+++ restart.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo '开始拉取git代码'
+
+git fetch --all
+git reset --hard origin/master
+git pull
+
+echo 'git 代码拉取完毕 开始安装node_modules'
+npm i
+
+echo 'node_modules安装完毕 开始build'
+
+npm run build
+
+echo 'build完成 开始重启pm2'
+
+pm2 reload pm2.config.json
