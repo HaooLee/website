54bc1234b8c04163c426322a88a6c7fe06ed0052
lihao <lihao@fanqiepinpin.com>
2020-04-10 15:36:22 +0800

    case

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
+const RouterServer = require('./server/index')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const server = new RouterServer()
+
+
+server.router('/case/:type', (req, res) => {
+  app.render(req, res, '/case', req.query)
+})
+server.router(/.*/, (req, res) => {
+  const parsedUrl = parse(req.url, true)
+  handle(req, res, parsedUrl)
+})
+
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
-
-    // if (pathname === '/a') {
-    //   app.render(req, res, '/b', query)
-    // } else if (pathname === '/b') {
-    //   app.render(req, res, '/a', query)
-    // } else {
-      handle(req, res, parsedUrl)
-  }).listen(3000, err => {
+  server.listen(3000, err => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:3000 App mode ${process.env.NODE_ENV}`)
+    console.log(`> Ready on http://localhost:3000; App mode: ${process.env.HHB_FE_MODE}`)
   })
+
+  // createServer((req, res) => {
+  //   // Be sure to pass `true` as the second argument to `url.parse`.
+  //   // This tells it to parse the query portion of the URL.
+  //   const parsedUrl = parse(req.url, true)
+  //   const {pathname, query} = parsedUrl
+  //
+  //   if (pathname === '/case/1') {
+  //     app.render(req, res, '/case', query)
+  //   }else {
+  //     handle(req, res, parsedUrl)
+  //   }
+  // }).listen(3000, err => {
+  //   if (err) throw err
+  //   console.log(`> Ready on http://localhost:3000 App mode ${process.env.NODE_ENV}`)
+  // })
 })
