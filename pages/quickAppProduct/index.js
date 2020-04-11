eb02cc45e3937d57a3e23b52f0ec8597917bdda7
liushuai <liushuai@teddymobile.cn>
2020-04-11 10:31:33 +0800

    sms

--- pages/quickAppProduct/index.js
+++ pages/quickAppProduct/index.js
@@ -90,54 +90,82 @@ export default class QuickAppProduct extends React.Component {
         }
       ],
       tabBarList: [{
-        name: '文本消息',
+        name: '全局搜索',
         active: true,
-        type: 'text'
+        type: 'global-search'
       }, {
-        name: '卡片消息',
+        name: '负一屏',
         active: false,
-        type: 'card'
+        type: 'hi-board'
       }, {
-        name: '多图文消息',
+        name: '语音助手',
         active: false,
-        type: 'more-pic-text'
+        type: 'siri'
       }, {
-        name: '快应用消息',
+        name: '厂商专属推荐',
         active: false,
-        type: 'quick-app'
+        type: 'firm'
+      }, {
+        name: '智能短信',
+        active: false,
+        type: 'sms'
+      }, {
+        name: '场景消息',
+        active: false,
+        type: 'scenic'
       }],
-      activeType: 'text'
+      activeType: 'global-search',
+      
     }
+    
     this.barContent = {
-      'text': {
-        src: '/static/images/scenicProduct/card.png',
-        name: '文本消息',
+      'global-search': {
+        src: '/static/images/quickAppProduct/全局搜索.png',
+        name: '全局搜索',
         desc: <div className="apply-desc__content">
           <p>低成本，多字数，突破传统短信70字符限制，满足企业长短信需求</p>
           <p>厂商报备，过审无拦截</p>
           <p>企业认证，品牌曝</p>
         </div>
       },
-      'card': {
-        src: '/static/images/scenicProduct/card.png',
-        name: '卡片消息',
+      'hi-board': {
+        src: '/static/images/quickAppProduct/负一屏.png',
+        name: '负一屏',
         desc: <div className="apply-desc__content">
           <p>优化展现形式，场景化呈现</p>
           <p>降低用户阅读成本，提升用户体验</p>
           <p>搭配按钮增加业务入口，提高用户转化</p>
         </div>
       },
-      'more-pic-text': {
-        src: '/static/images/scenicProduct/more-pic-text.png',
-        name: '多图文消息',
+      'siri': {
+        src: '/static/images/quickAppProduct/语音助手.png',
+        name: '语音助手',
         desc: <div className="apply-desc__content">
           <p>多图文展现形式，视觉体验佳</p>
           <p>展示形态满足用户点击习惯，转化效果好</p>
         </div>
       },
-      'quick-app': {
-        src: '/static/images/scenicProduct/quick-app.png',
-        name: '快应用消息',
+      'firm': {
+        src: '/static/images/quickAppProduct/厂商专属推荐.png',
+        name: '厂商专属推荐',
+        desc: <div className="apply-desc__content">
+          <p>基于用户场景，下发快应用消息</p>
+          <p>短链路，强留存</p>
+          <p>一键直达快应用</p>
+        </div>
+      },
+      'sms': {
+        src: '/static/images/quickAppProduct/智能短信.png',
+        name: '智能短信',
+        desc: <div className="apply-desc__content">
+          <p>基于用户场景，下发快应用消息</p>
+          <p>短链路，强留存</p>
+          <p>一键直达快应用</p>
+        </div>
+      },
+      'scenic': {
+        src: '/static/images/quickAppProduct/场景消息.png',
+        name: '场景消息',
         desc: <div className="apply-desc__content">
           <p>基于用户场景，下发快应用消息</p>
           <p>短链路，强留存</p>
@@ -162,6 +190,7 @@ export default class QuickAppProduct extends React.Component {
   }
   render() {
     const {coreList, products, industryList, tabBarList, activeType} = this.state
+    const types = ['global-search', 'hi-board', 'siri', 'firm', 'sms', 'scenic']
     return (
       <>
         <ProductBanner title={'快应用'} desc={'无需下载安装，即点即用，享受原生应用的性能体验'} src={'/static/images/quickAppProduct/banner.png'} customClassName={'quick-app-banner'}></ProductBanner>
@@ -179,21 +208,25 @@ export default class QuickAppProduct extends React.Component {
         </SectionCard> */}
         <SectionCard title={'应用形态'} bgc={'#F8FAFF'}>
           <div>
-              <TabBarCard list={tabBarList} customClassName={'scenic-tab-bar'} clickCallback={this.clickCallback}></TabBarCard>
-              <div className="tab-content clearfix">
-                  <div className="tab-content__left">
-                    <div className="tab-content__left__desc">
-                      {this.barContent[activeType] ? this.barContent[activeType].desc : ''}
-                      <div className="tab-content__left__btn">马上合作</div>
-                    </div>
+              <TabBarCard list={tabBarList} customClassName={'quick-tab-bar'} clickCallback={this.clickCallback}></TabBarCard>
+              {
+                types.map((item, idx) => (
+                  <div className="tab-content clearfix" key={idx} style={{display: item === activeType ? 'block' : 'none'}}>
+                      <div className="tab-content__left">
+                        <div className="tab-content__left__desc">
+                          {this.barContent[item] ? this.barContent[item].desc : ''}
+                          <div className="tab-content__left__btn">马上合作</div>
+                        </div>
+                      </div>
+                      <div className="tab-content__right clearfix">
+                        <div className="tab-content__right__item">
+                          <img src={this.barContent[item].src} />
+                          <p>{this.barContent[item].name}</p>
+                        </div>
+                      </div>
                   </div>
-                  <div className="tab-content__right clearfix">
-                    <div className="tab-content__right__item">
-                      <img src={this.barContent[activeType].src} />
-                      <p>{this.barContent[activeType].name}</p>
-                    </div>
-                  </div>
-              </div>
+                ))
+              }
           </div>
         </SectionCard>
         <SectionCard title={'产品优势'} bgc={'#fff'}>
