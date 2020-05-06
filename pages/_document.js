050a42e14286a10ab6525657ccd8266831509404
lihao <lihao@fanqiepinpin.com>
2020-05-06 10:56:49 +0800

    s

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
       <Head>
         <link rel="stylesheet" href="/static/css/restcss.css"/>
         <link type="favicon" rel="shortcut icon" href="/static/images/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+SC:100,300,400,500,700,900" />
         <link href="https://cdn.bootcss.com/slick-carousel/1.6.0/slick.min.css" rel="stylesheet" />
         <link href="https://cdn.bootcss.com/slick-carousel/1.6.0/slick-theme.min.css" rel="stylesheet" />
         <script src="http://g.tbcdn.cn/mtb/lib-flexible/0.3.4/??flexible_css.js,flexible.js"></script>
