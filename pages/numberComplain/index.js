0c760bbdd054f50d41dda9264a91cf7fe2543aa1
liushuai <liushuai@teddymobile.cn>
2020-05-09 18:19:39 +0800

    numberComplain

--- pages/numberComplain/index.js
+++ pages/numberComplain/index.js
@@ -18,9 +18,59 @@ export default class NumberComplain extends Component {
         }
       ],
       activeType: 'company',
-      fileContent: {}
+      fileContent: {},
+      companyErrors: {}
     }
   }
+
+  companyValues = {}
+
+  companyRules = {
+    'name': [{required:true,msg:'申诉企业不能为空'}],
+    'phone': [{required:true,msg:'申诉号码不能为空'}],
+    'contactPhone': [{required:true,msg:'联系方式不能为空'}],
+    'contactOther': [{required:true,msg:'联系邮箱/QQ不能为空'}],
+    'reason': [{required:true,msg:'申诉理由不能为空'}]
+  }
+
+  validate = (data, rules, type) =>{
+    let isValid = true
+    try {
+      const {companyErrors} = this.state
+      let errors = companyErrors
+      Object.entries(rules).forEach((item,index)=>{
+        item[1].forEach((i)=>{
+          if(i.required){
+            if(!data[item[0]]) {
+              errors[item[0]] = {err:true,msg:i.msg}
+              isValid = false
+            }else {
+              errors[item[0]] = {err:false}
+            }
+          }
+          if(i.validator) {
+            i.validator(data[item[0]], (msg) => {
+              if(msg) {
+                errors[item[0]] = {err:true,msg: msg}
+                isValid = false
+              } else {
+                errors[item[0]] = {err:false}
+              }
+            })
+          }
+        })
+      })
+      console.log(errors)
+      this.setState({
+        companyErrors: {...errors}
+      })
+    }catch (e) {
+
+      return false
+    }
+    return isValid
+  }
+
   headerItemClick = (idx, row) => {
     const {headers} = this.state 
     headers.forEach((item, index) => {
@@ -44,8 +94,18 @@ export default class NumberComplain extends Component {
       fileContent
     })
   }
+  companyFormChange = (type, ev) => {
+    const val = ev.currentTarget.value 
+    this.companyValues[type] = val 
+  }
+  companyFormSubmit = () => {
+    console.log(this.companyValues)
+    if(this.validate(this.companyValues, this.companyRules)){
+      console.log(this.companyValues)
+    }
+  }
   render() {
-    const {headers, activeType, fileContent} = this.state
+    const {headers, activeType, fileContent, companyErrors} = this.state
     return (
       <>
         <div className="banner">
@@ -65,10 +125,11 @@ export default class NumberComplain extends Component {
             </div>
             <div className="complain__content">
              {activeType === 'company' &&  <div className="form">
-                <div className="form__item">
+                <div className="form__item form__item--required">
                   <div className="form__item__label">申诉号码</div>
                   <div className="form__item__input">
-                    <input placeholder="输入号码，如果是座机号请加上区号" type="text" />
+                    <input placeholder="输入号码，如果是座机号请加上区号" onChange={this.companyFormChange.bind(this, 'phone')} type="text" />
+                    {companyErrors['phone']?.err && <span className={'errMsg'}>{companyErrors['phone'].msg}</span>}
                   </div>
                 </div>
                 <div className="form__item">
@@ -122,38 +183,42 @@ export default class NumberComplain extends Component {
                     </div>
                   </div>  
                 </div>
-                <div className="form__item form__item--no-margin">
+                <div className="form__item form__item--no-margin form__item--required">
                   <div className="form__item__label">申诉企业全称</div>
                   <div className="form__item__input">
-                    <input placeholder="请输入您的企业全称" type="text" />
+                    <input placeholder="请输入您的企业全称" type="text" onChange={this.companyFormChange.bind(this, 'name')} />
+                    {companyErrors['name']?.err && <span className={'errMsg'}>{companyErrors['name'].msg}</span>}
                   </div>
                 </div>
-                <div className="form__item">
+                <div className="form__item form__item--required">
                   <div className="form__item__label">申诉原因</div>
                   <div className="form__item__input">
-                    <input placeholder="请说明您的申诉原因" type="text" />
+                    <input placeholder="请说明您的申诉原因" type="text" onChange={this.companyFormChange.bind(this, 'reason')} />
+                    {companyErrors['reason']?.err && <span className={'errMsg'}>{companyErrors['reason'].msg}</span>}
                   </div>
                 </div>
-                <div className="form__item">
+                <div className="form__item form__item--required">
                   <div className="form__item__label">联系方式</div>
                   <div className="form__item__input">
-                    <input placeholder="请留下您的联系电话，以便我们能够及时为您提供服务" type="text" />
+                    <input placeholder="请留下您的联系电话，以便我们能够及时为您提供服务" type="text" onChange={this.companyFormChange.bind(this, 'contactPhone')} />
+                    {companyErrors['contactPhone']?.err && <span className={'errMsg'}>{companyErrors['contactPhone'].msg}</span>}
                   </div>
                 </div>
-                <div className="form__item">
+                <div className="form__item form__item--required">
                   <div className="form__item__label">联系邮箱/QQ</div>
                   <div className="form__item__input">
-                    <input placeholder="请留下您的联系邮箱及QQ" type="text" />
+                    <input placeholder="请留下您的联系邮箱及QQ" type="text" onChange={this.companyFormChange.bind(this, 'contactOther')} />
+                    {companyErrors['contactOther']?.err && <span className={'errMsg'}>{companyErrors['contactOther'].msg}</span>}
                   </div>
                 </div>
-                <div className="form__item">
+                <div className="form__item" onClick={this.companyFormSubmit}>
                   <div className="form__item__btn">确认提交</div>
                 </div>
               </div>}
 
               {
                 activeType === 'personal' &&  <div className="form">
-                <div className="form__item">
+                <div className="form__item form__item--required">
                   <div className="form__item__label">申诉号码</div>
                   <div className="form__item__input">
                     <input placeholder="输入号码，如果是座机号请加上区号" type="text" />
@@ -166,19 +231,19 @@ export default class NumberComplain extends Component {
                     <a className="form__item__input__code">获取验证码</a>
                   </div>
                 </div>
-                <div className="form__item">
+                <div className="form__item form__item--required">
                   <div className="form__item__label">申诉人姓名</div>
                   <div className="form__item__input">
                     <input placeholder="请填写您的姓名" type="text" />
                   </div>
                 </div>
-                <div className="form__item">
+                <div className="form__item form__item--required">
                   <div className="form__item__label">申诉原因</div>
                   <div className="form__item__input">
                     <input placeholder="请说明您的申诉原因" type="text" />
                   </div>
                 </div>
-                <div className="form__item">
+                <div className="form__item form__item--required">
                   <div className="form__item__label">联系方式</div>
                   <div className="form__item__input">
                     <input placeholder="请留下您的联系电话，以便我们能够及时为您提供服务" type="text" />
