2bb081f665b41aa90c7a003760e861b7776088c4
lihao <lihao@fanqiepinpin.com>
2020-05-28 17:10:59 +0800

    s

---
+++ static/js/statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?174cb42f4d816b37e2548f4c95cc76bb";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})()
